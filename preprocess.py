9c51071a517549142b64e4873e5def00cab1f5cf
PrasanthbabuKathi <97211944+PrasanthbabuKathi@users.noreply.github.com>
2025-04-11 11:50:42 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,15 @@
 # preprocess.py
 print("Step 1: Preprocessing data...")
-with open("data.txt", "w") as f:
+filename = "data.txt"
+with open(filename, "w") as f:
     f.write("processed data")
 print("Step 1 is compleated and created the file")
+try:
+    with open(filename, 'r') as file:
+        content = file.read()
+        print("File Content:\n")
+        print(content)
+except FileNotFoundError:
+    print(f"Error: The file '{filename}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
